72f1f3495b0569d1f7494c6bcd63abdda02f5f77
vantu2801 <nguyenvantu28011998@gmail.com>
2021-07-06 17:26:00 +0700

    day 2

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-	os.Setenv("ChannelAccessToken", "WaWrpzl9GkEBbMl0nMgAi7e+hqiUwvlYC6x1Uv1fbqMOBqEINdbcB4elBy8PJAfN2V+gs2eEkZtPvrrbuhrNm8TrmDf+l1BE2jdIBDPzVJMtcooXSRbax1PBqvScLAmAbxN/abn+NKUJ92nxUHN2yAdB04t89/1O/w1cDnyilFU=")
-	os.Setenv("ChannelSecret", "9208ccd3e9780d150c0aa8f009fff9a2")
+	os.Setenv("ChannelAccessToken", "FE+w3+zruEZ0e91TbusoHnOY0pUfa4kY5u4CGpqjoDybwRFy3JphtzNt7S6ciV2NCvuhNktPcZjOa6SrO/6dihlQ+Sd1b8dfdxkvsCEn2Y3P/HdyemMawGln8WBZWY9F7MPw/El3G5EQ3HqMTd2G2wdB04t89/1O/w1cDnyilFU=")
+	os.Setenv("ChannelSecret", "5e6ce8d78756dc46b6f75f5aefbad7b0")
 	os.Setenv("PORT", "8080")
 	bot, err := linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	if err != nil {
@@ -18,7 +18,7 @@ func main() {
 	}
 	fmt.Print("SUCCESSFULLY CREATED LINE BOT")
 
-	if _, err := bot.PushMessage("Ua3c33fd6a14f54421fbd1835cba1a487", linebot.NewTextMessage("Hello Da Nang")).Do(); err != nil {
+	if _, err := bot.PushMessage("U1bf6bc60d2e9e9fd5b71cd01e7db10a6", linebot.NewTextMessage("Hello !!! This is Go team's bot.")).Do(); err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("API OK")
